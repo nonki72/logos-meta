c6ea478a5762cbb0e80e2b4eed152b02f62ca08f
Jordan Cote <nonki720@gmail.com>
2023-07-27 17:46:42 +0000

    Revert "sample code for messenger chat"
    This reverts commit ca78e99bfd7e18626d381cf08a8ad09a68fa27a9.

--- app.js
+++ app.js
@@ -1,90 +1,75 @@
-/**
- * Copyright 2017-present, Facebook, Inc. All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// Import dependencies and set up http server
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  { urlencoded, json } = require("body-parser"),
+  app = express().use(bodyParser.json());
 
-// ===== MODULES ===============================================================
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import express from 'express';
-import favicon from 'serve-favicon';
-import logger from 'morgan';
-import path from 'path';
+const port = 8888;
 
-// ===== MESSENGER =============================================================
-import ThreadSetup from './messenger-api-helpers/thread-setup';
 
-// ===== ROUTES ================================================================
-import index from './routes/index';
-import users from './routes/users';
-import webhooks from './routes/webhooks';
+app.post("/webhook", verifyRequestSignature, (req, res) => {
+  let body = req.body;
 
-const app = express();
+  console.log(`\u{1F7EA} Received webhook:`);
+  console.dir(body, { depth: null });
+    
+  // Send a 200 OK response if this is a page webhook
 
-/* =============================================
-   =           Basic Configuration             =
-   ============================================= */
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    res.status(200).send("EVENT_RECEIVED");
+//...
+    // Determine which webhooks were triggered and get sender PSIDs and locale, message content and more.
+    console.log(JSON.stringify(body));
+//...
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+}); 
 
-app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// Add support for GET requests to our webhook
+app.get("/messaging-webhook", verifyRequestSignature, (req, res) => {
+  
+// Parse the query params
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
 
-/* ----------  Views  ---------- */
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-/* ----------  Static Assets  ---------- */
-
-app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-/* ----------  Parsers  ---------- */
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-
- /* ----------  Loggers &c  ---------- */
-
-app.use(logger('dev'));
-
-/* =============================================
-   =                   Routes                  =
-   ============================================= */
-
-/* ----------  Primary / Happy Path  ---------- */
-
-app.use('/', index);
-app.use('/webhook', webhooks);
-app.use('/users', users);
-
-/* ----------  Errors  ---------- */
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === config.verifyToken) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Respond with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
 });
 
-app.use(function(err, req, res) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Verify that the callback came from Facebook.
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature-256"];
 
-/* ----------  Messenger setup  ---------- */
-ThreadSetup.setGetStarted();
+  if (!signature) {
+    console.warn(`Couldn't find "x-hub-signature-256" in headers.`);
+  } else {
+    var elements = signature.split("=");
+    var signatureHash = elements[1];
+    var expectedHash = crypto
+      .createHmac("sha256", config.appSecret)
+      .update(buf)
+      .digest("hex");
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
 
-app.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port')); // eslint-disable-line
-});
-
-export default app;
+app.listen(port, () => {
+  console.log(`Logos Meta bot listening on port ${port}`)
+})
